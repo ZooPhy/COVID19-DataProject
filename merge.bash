1e8489ee145b3f7ab83301ee5b1e8ded59c697c2
masauer2 <masauer2@asu.edu>
2020-11-14 21:59:23 -0700

    Revert "Merge pull request #1 from ZooPhy/masauer2-climatedata"
    This reverts commit 6fd119d7400e71f5dd7e3421e4518b54b71b4e52, reversing
    changes made to 208117a2ec6a282e68d928127aa5459d7af05392.

--- merge.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-sort -t , -k 1,1 export_final.csv > sortData.csv #Sort the Temp data by station ID
-join -t , -1 1 -2 1 sortData.csv STATION_DATA.csv > mergedDataTEMPLOCATIONV2.csv #Merge based on station ID
-#cut -d, -f 2,8,9 mergedSTATIONNAME_COUNTY.csv > mergedSTATIONNAME_COUNTYV2.csv
-#sed 's/*,/,/g' mergedSTATIONNAME_COUNTYV2.csv > mergedSTATIONNAME_COUNTYV3.csv 
-#join -t , -1 1 -2 1 mergedDataTEMPLOCATIONV2.csv mergedSTATIONNAME_COUNTYV3.csv > mergedDataTEMPLOCATIONV3.csv #Merge based on station ID
-#sed 's/*,/,/g' mergedDataTEMPLOCATIONV3.csv
