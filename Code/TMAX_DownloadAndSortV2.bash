46c9b04cfc1f29eaf33ff8d93c3dc09cb9a1c82e
masauer2 <49538892+masauer2@users.noreply.github.com>
2020-11-26 01:07:18 -0700

    Update TMAX_DownloadAndSortV2.bash

--- Code/TMAX_DownloadAndSortV2.bash
+++ Code/TMAX_DownloadAndSortV2.bash
@@ -13,3 +13,5 @@ awk -F, '{new_var=$1"\t"$2; print new_var "," $3}' 2020_TMAX_USA_SORTED.csv >F1.
 awk -F, '{new_var=$1"\t"$2; print new_var "," $3}' 2020_PRCP_USA_SORTED.csv >F2.csv
 join -t, -1 1 -2 1 F1.csv F2.csv > downloaded_export.csv
 awk -F'\t' '{printf "%s,%s\n",$1,$2}' downloaded_export.csv > export_final.csv
+sort -t , -k 1,1 export_final.csv > sortData.csv #Sort the Temp data by station ID
+join -t , -1 1 -2 1 sortData.csv STATION_DATA.csv > mergedDataTEMPLOCATIONV2.csv #Merge based on station ID
