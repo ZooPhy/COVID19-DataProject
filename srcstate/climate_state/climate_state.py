bd3fbd77a3e230b90fc1e20aa1c7b8dd2505c084
masauer2 <49538892+masauer2@users.noreply.github.com>
2021-01-02 03:39:31 -0700

    Add files via upload

---
+++ srcstate/climate_state/climate_state.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+
+collist = ["Station", "Date", "Temp", "Prcp", "County", "FIPS"]
+df = pd.read_csv('climateDataDownload.csv', names = collist, dtype={'FIPS': object}, sep=',')
+FIPSMetadata = pd.read_csv('./../covid_state/FIPSMetadata.csv', dtype={'FIPS': object}, sep=',')
+df = df.merge(FIPSMetadata)
+print(df)
+temp_median = df.groupby('State', as_index=False)['Temp'].median()
+temp_mean = df.groupby('State', as_index=False)['Temp'].mean()
+prcp_median = df.groupby('State', as_index=False)['Prcp'].median()
+prcp_mean = df.groupby('State', as_index=False)['Prcp'].mean()
+
+#Rename columns
+temp_median_rename = temp_median.rename(columns={"Temp":"TempMedian"})
+prcp_median_rename = prcp_median.rename(columns={"Prcp":"PrcpMedian"})
+temp_mean_rename = temp_mean.rename(columns={"Temp":"TempMean"})
+prcp_mean_rename = prcp_mean.rename(columns={"Prcp":"PrcpMean"})
+
+#Merge the columns based on FIPS code
+
+tempMerge = temp_median_rename.merge(temp_mean_rename)
+prcpMerge = tempMerge.merge(prcp_median_rename)
+climateMerge = prcpMerge.merge(prcp_mean_rename)
+climateMerge.to_csv('climateData.csv', index=False)
