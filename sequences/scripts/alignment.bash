2b6fc1341f3a3123ff18689356f0b2937c9ac230
masauer2 <49538892+masauer2@users.noreply.github.com>
2021-04-30 14:11:21 -0700

    Update alignment.bash

--- sequences/scripts/alignment.bash
+++ sequences/scripts/alignment.bash
@@ -4,4 +4,4 @@ wget https://mafft.cbrc.jp/alignment/software/mafft_7.475-1_amd64.deb
 sudo dpkg -i mafft_7.475-1_amd64.deb
 which mafft
 mafft --version
-mafft sequences.fasta > alignedSequences.fasta
+mafft sequences.fasta > sequences_aln.fasta
