2bc0e22376ba89d33316c3a79a26b13df086d060
masauer2 <49538892+masauer2@users.noreply.github.com>
2021-08-04 21:33:13 -0700

    Add files via upload

---
+++ codebase/run.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import os
+os.system('cd climate && bash ./downloadClimateData.bash')
+os.system('cd COVID-19 && python covid.py')
+os.system('cd travel && python travel.py')
+os.system('cd climate && python climate.py')
+os.system('cd vaccine && python vaccine.py')
+os.system('python merge.py')
+os.system('mv 2021* ./data')
+os.system('python merge2.py')
+os.system('cd climate && rm 2020* && rm F-* && rm sortedClimateDownload.csv')
